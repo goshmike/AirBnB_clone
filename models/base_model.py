a9929cc1e743be25015ad398c384fd176d6ac356
goshmike <goshenconsultsng@gmail.com>
2023-12-11 21:25:33 +0100

    AirBnb

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Module for base class
+Contains the base class for the AirBnB clone console.
+"""
+
+import uuid
+import datetime
+import models
+
+
+class BaseModel:
+
+        """Class for base model of object hierarchy."""
+
+            def __init__(self, *args, **kwargs):
+                        """Initialization of a Base instance.
+
+                                Args:
+                                            - *args: list of arguments
+                                                        - **kwargs: dict of key-values arguments
+                                                                """
+
+                                                                        if kwargs:
+                                                                                        for name, value in kwargs.items():
+                                                                                                            if name != '__class__':
+                                                                                                                                    if name == 'created_at' or name == 'updated_at':
+                                                                                                                                                                value = datetime.datetime.strptime(
+                                                                                                                                                                                                    value, "%Y-%m-%dT%H:%M:%S.%f")
+                                                                                                                                                                                    setattr(self, name, value)
+                                                                                                                                                                                            else:
+                                                                                                                                                                                                            self.id = str(uuid.uuid4())
+                                                                                                                                                                                                                        self.created_at = datetime.datetime.now()
+                                                                                                                                                                                                                                    self.updated_at = self.created_at
+                                                                                                                                                                                                                                                models.storage.new(self)
+
+                                                                                                                                                                                                                                                    def __str__(self):
+                                                                                                                                                                                                                                                                """Returns a human-readable string representation
+                                                                                                                                                                                                                                                                        of an instance."""
+
+                                                                                                                                                                                                                                                                                return "[{}] ({}) {}".\
+                                                                                                                                                                                                                                                                                                    format(type(self).__name__, self.id, self.__dict__)
+
+                                                                                                                                                                                                                                                                                                        def save(self):
+                                                                                                                                                                                                                                                                                                                    """Updates the updated_at attribute
+                                                                                                                                                                                                                                                                                                                            with the current datetime."""
+
+                                                                                                                                                                                                                                                                                                                                    self.updated_at = datetime.datetime.now()
+                                                                                                                                                                                                                                                                                                                                            models.storage.save()
+
+                                                                                                                                                                                                                                                                                                                                                def to_dict(self):
+                                                                                                                                                                                                                                                                                                                                                            """Returns a dictionary representation of an instance."""
+
+                                                                                                                                                                                                                                                                                                                                                                    my_dict = dict(self.__dict__)
+                                                                                                                                                                                                                                                                                                                                                                            my_dict["__class__"] = type(self).__name__
+                                                                                                                                                                                                                                                                                                                                                                                    my_dict["created_at"] = my_dict["created_at"].isoformat()
+                                                                                                                                                                                                                                                                                                                                                                                            my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+                                                                                                                                                                                                                                                                                                                                                                                                    return my_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Class FileStorage"""
+
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import json
+
+
+class FileStorage:
+        """
+            class that serializes instances to a JSON file
+                and deserializes JSON file to instances:
+                    """
+
+                        __file_path = "file.json"
+                            __objects = {}
+
+                                def all(self):
+                                            """Public instance, return dictionary"""
+                                                    return self.__objects
+
+                                                    def new(self, obj):
+                                                                """sets in __objects the obj with key <obj class name>.id"""
+                                                                        if obj:
+                                                                                        key = '{}.{}'.format(type(obj).__name__, obj.id)
+                                                                                                    self.__objects[key] = obj
+
+                                                                                                        def save(self):
+                                                                                                                    """Serializes __objects to the JSON file (path: __file_path)"""
+                                                                                                                            new_dict = {}
+                                                                                                                                    for key, value in self.__objects.items():
+                                                                                                                                                    new_dict[key] = value.to_dict()
+
+                                                                                                                                                            json_str = json.dumps(new_dict)
+
+                                                                                                                                                                    with open(self.__file_path, mode="w+", encoding="utf-8") as file:
+                                                                                                                                                                                    file.write(json_str)
+
+                                                                                                                                                                                        def reload(self):
+                                                                                                                                                                                                    """Deserializes the JSON file to __objects"""
+                                                                                                                                                                                                            try:
+                                                                                                                                                                                                                            with open(self.__file_path, mode="r", encoding="utf-8") as file:
+                                                                                                                                                                                                                                                json_string = json.load(file)
+                                                                                                                                                                                                                                                                for key in json_string.values():
+                                                                                                                                                                                                                                                                                        clas = key["__class__"]
+                                                                                                                                                                                                                                                                                                            self.new(eval("{}({})".format(clas, '**key')))
+                                                                                                                                                                                                                                                                                                                    except FileNotFoundError:
+                                                                                                                                                                                                                                                                                                                                    pass
